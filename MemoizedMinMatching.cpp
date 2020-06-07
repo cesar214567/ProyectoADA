73fb86745f81f45159567234cede623df45748be
cesar214567 <cesar.madera@utec.edu.pe>
2020-06-07 14:11:30 -0500

    pseudo for memoized

--- MemoizedMinMatching.cpp
+++ MemoizedMinMatching.cpp
@@ -1,71 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <limits.h>
-#include <utility>
-#define p 9 
-using namespace std;
+pair<vector<Tupla>, double> cuadA[p][p][p]={0};
+pair<vector<Tupla>, double> cuadB[p][p][p]={0};
+pair<vector<Tupla>, double> respuestas[p][p]; //inicializar los .second con INT16_MAX;
 
-class bloque{
-public:
-    int longitud;
-    int start;
-    int end;
-    bloque(int start_,int end_, int longitud_):start{start_}, end{end_},longitud{longitud_}{} 
-    bloque(int l): longitud{l}{}
-};
 
-class Tupla{
-public:
-    int first;
-    int second;
-    Tupla(int first, int second){
-        this->first = first;
-        this->second = second;
+min_peso_bloques(r,s,i,j){
+    if (r == s){
+        if (cuadA[r][i][j].second==INT16_MAX){
+            cuadA[r][i][j].second=0;
+            for(int m = i; m <= j; m++) {
+                cuadA[r][i][j].first.emplace_back( r, m );
+                cuadA[r][i][j].second += B[m].longitud;
+            }
+        }
+        return make_pair ( cuadA[r][i][j].first , A[r].longitud/cuadA[r][i][j].second );
+
+    
     }
-};
-
-//A = [ 0, 1 , 0 , 0 , 1 , 1, 0 , 1 , 1]
-//B = [ 0, 0,  1 , 1 , 0 , 1 , 1, 0 , 1]
-
-vector<bloque> A;
-vector<bloque> B;
-
-pair<vector<Tupla>, double> min_peso_bloques(int r , int s, int i, int j){
-    if ( r == s ){
-        vector<Tupla> matchs;
-        double suma = 0.0;
-
-        for(int m = i; m <= j; m++) {
-            matchs.emplace_back( r, m );
-            suma += B[m].longitud;
+    else if (i == j){
+        if (cuadB[i][r][s]==INT16_MAX){
+            cuadB[i][r][s].second=0;
+            for(int m = r; m <= s; m++) {
+                cuadB[i][r][s].first.emplace_back( m, i );
+                cuadB[i][r][s].second += B[m].longitud;
+            }
         }
-        return make_pair ( matchs , A[r].longitud/suma );
-
-    }else if ( i == j ){
-        vector<Tupla> matchs;
-        double suma = 0.0;
-
-        for(int m = r; m <= s; m++) {
-            matchs.emplace_back( m, i );
-            suma += A[m].longitud;
-        }
-        return make_pair ( matchs , suma/B[i].longitud );
-    } else{
-        double min_resultk = INT16_MAX;
-        double min_resultl = INT16_MAX;
-
-        double resultk, resultl;
-
-        pair<vector<Tupla>,double> leftk;
-        pair<vector<Tupla>,double> rightk;
-
-        pair<vector<Tupla>,double> leftl;
-        pair<vector<Tupla>,double> rightl;
-
-        vector<Tupla> matchsk;
-        vector<Tupla> matchsl;
-
+        return make_pair ( cuadB[i][r][s].first , A[r].longitud/cuadB[i][r][s].second );
+    }
+    else{
+        .
+        .
+        .
         for(int k = j-1; k >=i ; k--){
             auto aux_leftk = min_peso_bloques(r, r, i, k);
             auto aux_rightk = min_peso_bloques(r+1, s, k+1, j);
@@ -89,66 +53,35 @@ pair<vector<Tupla>, double> min_peso_bloques(int r , int s, int i, int j){
                 min_resultl = resultl;
                 leftl = aux_leftl;
                 rightl = aux_rightl;
+
             }
         }
 
         matchsl.insert( matchsl.begin(), begin(leftl.first), end(leftl.first) );
         matchsl.insert( matchsl.end(), begin(rightl.first), end(rightl.first) );
 
-        if( min_resultk > min_resultl ) return make_pair( matchsl , min_resultl );
-        else return make_pair( matchsk , min_resultk );
+        if( min_resultk > min_resultl ) {
+            respuestas[i][j].first=matchsl;
+            respuestas[i][j].second=min_resultl;
+        }else{
+            respuestas[i][j].first=matchsk;
+            respuestas[i][j].second=min_resultk;
+        }
+        return respuestas[i][j];
+
+
+
+
+
+
+
+
+
 
     }
-}
-pair<vector<Tupla>, double> MIN_MATCHING(int a[], int b[]){
-  //A={0 1 1 1 0 0 1 1 }
-  int cont= 0; 
-  bool c = false; 
-  for(int i = 0; i < p ; i++){
-    if(a[i]){
-      cont++;
-      c = true;
-      if(i == p-1){
-        A.emplace_back(bloque(i-cont+1,i,cont));  
-      }
-    }
-    else if(cont){
-      A.emplace_back(bloque(i-cont,i-1,cont));
-      cont = 0;
-      c = false;
-    } 
-    
-  }
-  cont= 0; 
-  c = false; 
-  for(int i = 0; i < p ; i++){
-    if(b[i]){
-      cont++;
-      c = true;
-      if(i == p-1){
-        B.emplace_back(bloque(i-cont+1,i,cont));  
-      }
-    }
-    else if(cont){
-      B.emplace_back(bloque(i-cont,i-1,cont));
-      cont = 0;
-      c = false;
-    }   
-  }
-  return min_peso_bloques(0,A.size()-1, 0,B.size()-1) ;
-}
 
-int main() {
-  
-    int a[p]={0, 1 , 0 , 0 , 1 , 1, 0 , 1 , 0};
-    int b[p]={0, 0,  1 , 1 , 0 , 1 , 1, 0 , 1};
-    
-    auto result = MIN_MATCHING(a,b);
 
-    cout << "Optimo: " << result.second << endl;
-    for(auto v : result.first){
-        cout<<"("<<v.first<<","<<v.second<<") ";
-    }
-  
-    
+
+
+
 }
