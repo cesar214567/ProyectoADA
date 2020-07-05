696118e82ea88f1d19a5dde1652bf9fc768ed9f6
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-05 12:43:04 -0500

    Obtener matriz de Grises

--- pil.py
+++ pil.py
@@ -1,47 +1,80 @@
 from PIL import Image
 import os
 
+def LUM_601(imagen):
+    convert(imagen,0.299,0.587,0.114)
 
-os.remove("matriz01.txt")
-os.remove("matrizL.txt")
-image = Image.open("Abdullah.jpeg")
-xsize,ysize=image.size
-for x in range(10):
-    for y in range(10):
-        arr=[x,y]
-        tupla=tuple(arr)
-image = image.convert("L")
-image.show()
-file = open("matrizL.txt","w+")
-file2= open("matriz01.txt","w+")
-matriz0=[]
-matrizL=[]
-for x in range(xsize):
-    arr0=[]
-    arrL=[]
-    for y in range(ysize):
-        arr=[x,y]
-        tupla=tuple(arr)
-        arrL.append(image.getpixel(tupla))
-        if(image.getpixel(tupla)>122):
-            arr0.append(1)
-        else:
-            arr0.append(0)
-    matrizL.append(arrL)
-    matriz0.append(arr0)
+def LUM_709(imagen):
+    convert(imagen,0.2126,0.7152,0.0722)
 
-for x in range(xsize):
-    temporal=""
-    for y in range(ysize):
-        temporal=temporal+str(matrizL[x][y])+" "
-    temporal+="\n"
-    file.write(temporal)
+def LUM_240(imagen):
+    convert(imagen, 0.212, 0.701, 0.087)
 
-for x in range(xsize):
-    temporal=""
-    for y in range(ysize):
-        temporal=temporal+str(matriz0[x][y])+" "
-    temporal+="\n"
-    file2.write(temporal)
+def LUM_input(imagen): 
+    while(True):
+        R = float(input("Ingrese el coeficiente de R: "))
+        G = float(input("Ingrese el coeficiente de G: "))
+        B = float(input("Ingrese el coeficiente de B: "))
+        if( R+G+B == 1):
+            convert(imagen,R,G,B)
+            break
+        print("Los valores ingresados no son correctos. Ingrese nuevamente:")
+     
+
+def convert(imagen,R,G,B):
+    os.remove("matriz01.txt")
+    os.remove("matrizL.txt")
+   #image = Image.open("Abdullah.jpeg")
+    image = Image.open(imagen)
+    xsize,ysize=image.size
+
+    file = open("matrizL.txt","w+")
+    file2= open("matriz01.txt","w+")
+    
+    matriz0=[]
+    matrizL=[]
+    
+   # imgGris = image
+
+    for x in range(xsize):
+        arr0=[]
+        arrL=[]
+        for y in range(ysize):
+            RGB = image.getpixel((x,y))
+            Gris = int(RGB[0]*R + RGB[1]*G + RGB[2]*B)
+            #imgGris.putpixel((x,y),(Gris,Gris,Gris))
+            arrL.append(Gris)
+            #Politica si el gris es 1 o 0 
+            if(Gris > 122):
+                arr0.append(1)
+            else:
+                arr0.append(0)
+        matrizL.append(arrL)
+        matriz0.append(arr0)
+    
+    #imgGris.show()
+
+    for x in range(xsize):
+        temporal=""
+        for y in range(ysize):
+            temporal=temporal+str(matrizL[x][y])+" "
+        temporal+="\n"
+        file.write(temporal)
+
+    for x in range(xsize):
+        temporal=""
+        for y in range(ysize):
+            temporal=temporal+str(matriz0[x][y])+" "
+        temporal+="\n"
+        file2.write(temporal)
+        
+
+if __name__ == "__main__":
+
+    
+    imgPath =  "Abdullah.jpeg"
+    LUM_240(imgPath)
+    LUM_601(imgPath)
+    LUM_709(imgPath)      
     
 
