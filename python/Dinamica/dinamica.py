a720d0ee5d40cda6385b91134245d1270403a7b5
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-11 15:29:55 -0500

    fixing dinamica

--- python/Dinamica/dinamica.py
+++ python/Dinamica/dinamica.py
@@ -1,7 +1,9 @@
 import math
+sys.path.append("../")
+import Useful as us
 
 Matrix = []
-minSubProblem = {} //Para GetTuplas_1
+minSubProblem = {} #Para GetTuplas_1
 
 sumaBloquesA = []
 sumaBloquesB = []
@@ -10,27 +12,27 @@ A = []
 B = []
 TuplasOPT = []
 
-def GetMatchDivision( i, m, n ){
+def GetMatchDivision( i, m, n ):
 	if(m == n):
         return float(A[i].longitud)/B[m].longitud
 	if(m == 0):
         return float(A[i].longitud)/sumaBloquesB[n]
-	suma = sumaBloquesB[n] - sumaBloquesB[m-1];
+	suma = sumaBloquesB[n] - sumaBloquesB[m-1] 
 
-	return A[i].longitud/float(suma);
-}
+	return A[i].longitud/float(suma)
+ 
 
-def GetMatchGroup( r, s, j ){
+def GetMatchGroup( r, s, j ): 
 	if(r == s):
-        return A[r].longitud/float(B[j].longitud);
+        return A[r].longitud/float(B[j].longitud) 
 	if(r == 0):
-        return sumaBloquesA[s]/float(B[j].longitud);
-	suma = sumaBloquesA[s] - sumaBloquesA[r-1];
+        return sumaBloquesA[s]/float(B[j].longitud) 
+	suma = sumaBloquesA[s] - sumaBloquesA[r-1] 
 	
-	return float(suma)/B[j].longitud;
-}
+	return float(suma)/B[j].longitud 
+ 
 
-def OPT_Result( i, j ){
+def OPT_Result( i, j ): 
 	min_resultk = math.inf
     min_resultl = math.inf
 
@@ -57,12 +59,12 @@ def OPT_Result( i, j ){
 	
 	if(min_resultl > min_resultk):
 		Matrix[i][j] = min_resultk
-		minSubProblem[i,j] = (indexMinGroup-1, j-1); #Usado en GetTuplas_1
+		minSubProblem[i,j] = (indexMinGroup-1, j-1)  #Usado en GetTuplas_1
 	else:
-		Matrix[i][j] = min_resultl;
-		minSubProblem[i,j] = (i-1,indexMinDivision-1); #Usado en GetTuplas_1
+		Matrix[i][j] = min_resultl 
+		minSubProblem[i,j] = (i-1,indexMinDivision-1)  #Usado en GetTuplas_1
 
-def DynamicProgramming( x, y ){
+def DynamicProgramming( x, y ): 
 	#Casos Bases
 	for i in range(0, x):
         Matrix[i][0] = GetMatchGroup(0,i,0)
@@ -75,11 +77,11 @@ def DynamicProgramming( x, y ){
 
 	OPT_Result(x,y)
 	
-    return Matrix[x][y];
-}
+    return Matrix[x][y] 
+ 
 
-def GetTuplas(OPT){
-    SubProblem = minSubProblem[OPT];
+def GetTuplas(OPT): 
+    SubProblem = minSubProblem[OPT] 
 
     if(SubProblem[0] == 0):
         for j in range(0, SubProblem[1]+1):
@@ -89,7 +91,8 @@ def GetTuplas(OPT){
         for i in range(0, SubProblem[0]+1):
             TuplasOPT.append(i, SubProblem[1])
 
-    else GetTuplas(SubProblem)
+    else:
+        GetTuplas(SubProblem)
 
     if(SubProblem[0] + 1 == OPT[0]):
         for j in range(SubProblem[1]+1, OPT[1]+1):
@@ -98,30 +101,35 @@ def GetTuplas(OPT){
         for i in range(SubProblem[0]+1, OPT[0]+1):
             TuplasOPT.append(i, OPT[1])
 
-def MIN_MATCHING(a, b){
-	ObtenerBloques(A,a);
-	ObtenerBloques(B,b);
+def MIN_MATCHING(a, b): 
+	A = us.ObtenerBloques(a) 
+	B = us.ObtenerBloques(b) 
 	  
 	sumaBloquesA.append(A[0].longitud)
+    
     for i in range(1, len(A)):
         sumaBloquesA.append( A[i].longitud + sumaBloquesA[i-1] )
 
 	sumaBloquesB.append(B[0].longitud)
+    
     for i in range(1, len(B)):
         sumaBloquesB.append( B[i].longitud + sumaBloquesB[i-1] )
 
     for i in range(0, len(A)):
         Matrix.append([])
+
     for i in range(0, len(A)):
         Matrix[i] = [0]*len(B)
 
-	return DynamicProgramming( len(A)-1, len(B)-1 );
-}
+	return DynamicProgramming( len(A)-1, len(B)-1 ) 
+ 
 
-def main():
-    a = [0, 1 , 0 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 1 , 0 , 1 , 1 , 0 , 1]
-    b = [0, 0,  1 , 1 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0]
-    #Menu(a,b);
+if __name__ == "__main__":
+    #a = [0, 1 , 0 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 1 , 0 , 1 , 1 , 0 , 1]
+    #b = [0, 0,  1 , 1 , 0 , 1 , 1, 0 , 1 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0]
+    a = []
+    b = []
+    us.Menu(a,b) 
     print("OPTIMO: " + str(MIN_MATCHING(a,b)) + "\n")
     GetTuplas( ( len(A)-1, len(B)-1) )
 
@@ -129,3 +137,6 @@ def main():
     for it in TuplasOPT:
         print("(" + str(it[0]) + "," + str(it[1]) + ")", end=' ')
     print("")
+
+
+    
