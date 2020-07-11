043dd296e6cb5f48e46186047bacc86f4cfb1793
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-11 12:48:52 -0500

    deleted matrizL

--- pil.py
+++ pil.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 import os
 
 def LUM_601(imagen):
@@ -19,37 +20,34 @@ def LUM_input(imagen):
             convert(imagen,R,G,B)
             break
         print("Los valores ingresados no son correctos. Ingrese nuevamente:")
-     
+
+def InvertMatrix(mat):
+    m = np.matrix(mat)
+    return np.linalg.inv(m) 
 
 def convert(imagen,R,G,B):
     os.remove("matriz01.txt")
-    os.remove("matrizL.txt")
    #image = Image.open("Abdullah.jpeg")
     image = Image.open(imagen)
     xsize,ysize=image.size
 
-    file = open("matrizL.txt","w+")
-    file2= open("matriz01.txt","w+")
+    file= open("matriz01.txt","w+")
     
     matriz0=[]
-    matrizL=[]
-    
+
    # imgGris = image
 
     for x in range(xsize):
         arr0=[]
-        arrL=[]
         for y in range(ysize):
             RGB = image.getpixel((x,y))
             Gris = int(RGB[0]*R + RGB[1]*G + RGB[2]*B)
             #imgGris.putpixel((x,y),(Gris,Gris,Gris))
-            arrL.append(Gris)
             #Politica si el gris es 1 o 0 
             if(Gris > 122):
                 arr0.append(1)
             else:
                 arr0.append(0)
-        matrizL.append(arrL)
         matriz0.append(arr0)
     
     #imgGris.show()
@@ -57,24 +55,13 @@ def convert(imagen,R,G,B):
     for x in range(xsize):
         temporal=""
         for y in range(ysize):
-            temporal=temporal+str(matrizL[x][y])+" "
+            temporal=temporal+str(matriz0[x][y])+""
         temporal+="\n"
         file.write(temporal)
-
-    for x in range(xsize):
-        temporal=""
-        for y in range(ysize):
-            temporal=temporal+str(matriz0[x][y])+" "
-        temporal+="\n"
-        file2.write(temporal)
         
 
 if __name__ == "__main__":
-
-    
     imgPath =  "Abdullah.jpeg"
     LUM_240(imgPath)
-    LUM_601(imgPath)
-    LUM_709(imgPath)      
-    
-
+    #LUM_601(imgPath)
+    #LUM_709(imgPath)
