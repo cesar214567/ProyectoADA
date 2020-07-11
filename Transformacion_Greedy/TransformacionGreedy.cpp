5e293fce2d7eed7d32a51d66418fa1161ebde5fb
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-11 12:54:25 -0500

    changed for two different files

--- Transformacion_Greedy/TransformacionGreedy.cpp
+++ Transformacion_Greedy/TransformacionGreedy.cpp
@@ -1,40 +1,38 @@
 #include "../Secuencias/Greedy/greedy.cpp"
 #include "../Usefull2.cpp"
 
-
-
-
 int main(){
-    auto temp= getMatriz();
-    vector<vector<int>> matriz1;
-    vector<vector<int>> matriz2;
-    matriz1=temp.first;
-    matriz2=temp.second;
-    assert(matriz1.size()==matriz2.size());
+    auto mat1= getMatriz("matriz01.txt");
+    auto mat2= getMatriz("matriz02.txt");
+    vector <vector<int>> matriz1;
+    vector <vector<int>> matriz2;
+    assert(mat1.size() == mat2.size());
+
     for (auto it:matriz1){
-        for (auto it2:it){
-            cout<<it2<<" "; 
+        for (auto it2 : it){
+            cout << it2 <<" "; 
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl;
+    cout << endl;
+    
     for (auto it:matriz2){
         for (auto it2:it){
-            cout<<it2<<" "; 
+            cout << it2 << " "; 
         }
-        cout<<endl; 
+        cout << endl; 
     }
-    double sumatoria =0;
-    for (int i=0;i<matriz1.size();i++){
-        auto result = MIN_MATCHING(matriz1[i],matriz2[i]);
 
+    double sumatoria =0;
+    for (int i = 0; i < matriz1.size(); i++){
+        auto result = MIN_MATCHING(matriz1[i],matriz2[i]);
+        
         for(auto v : result.first){
-            cout<<"("<<v.first<<","<<v.second<<") ";
+            cout << "(" << v.first << "," << v.second <<") ";
         }
         cout << endl << result.second <<endl;
-        sumatoria+=result.second;
+        sumatoria += result.second;
     }
-    cout<<"LA SUMTORIA ES: "<<sumatoria<<endl;
-
+    cout << "LA SUMTORIA ES: " << sumatoria << endl;
 
 }
