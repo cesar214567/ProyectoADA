1e61945ce1c7bbd2729300926f20b4b7f58ee5e9
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-18 12:32:19 -0500

    Transformacion greedy python:

--- 2da Entrega/Transformacion_Greedy/TransformacionGreedy.py
+++ 2da Entrega/Transformacion_Greedy/TransformacionGreedy.py
@@ -1,41 +1,38 @@
-#include "../Secuencias/Dinamica/dinamica.cpp"
-
-import Useful
-
+import sys 
+sys.path.append("../")
+sys.path.append("../Greedy/")
+import Greedy as greed
+import Useful as us
 
 if __name__ == "__main__":
-    auto temp= getMatriz();
-    vector<vector<int>> matriz1;
-    vector<vector<int>> matriz2;
-    matriz1=temp.first;
-    matriz2=temp.second;
-    assert(matriz1.size()==matriz2.size());
-    for (auto it:matriz1){
-        for (auto it2:it){
-            cout<<it2<<" "; 
-        }
-        cout<<endl;
-    }
-    cout<<endl;
-    for (auto it:matriz2){
-        for (auto it2:it){
-            cout<<it2<<" "; 
-        }
-        cout<<endl; 
-    }
-    double sumatoria =0;
-    for (int i=0;i<matriz1.size();i++){
-        auto result = MIN_MATCHING(matriz1[i],matriz2[i]);
-        GetTuplas( make_pair( matriz1[i].size()-1, matriz2[i].size()-1) );
+    mat1 = us.getMatriz("matriz01.txt")
+    mat2 = us.getMatriz("matriz02.txt")
+    matriz1 = []
+    matriz2 = []
+    assert(mat1.size() == mat2.size())
 
-        cout << "Tuplas: ";
-        for (auto it : TuplasOPT){
-        cout<<"("<<it.first<<","<<it.second<<") ";
-        }
-        cout << endl;
-        sumatoria+=result;
-    }
-    cout<<"LA SUMTORIA ES: "<<sumatoria<<endl;
+    for  it in matriz1:
+        for it2  in it:
+            print(it2, end = " ") 
+        print("") 
+    print("")
+    
+    for it in matriz2:
+        for it2 in it:
+            print(it2, end = " ") 
+        print("") 
 
 
+    sumatoria =0
+
+    for i in range(0,len(matriz1)):
+        result = greed.MIN_MATCHING(matriz1[i],matriz2[i])
+
+        for v in result[0]:
+            print("(" + str(v[0] + "," + v[1] + ") " , end = ""))
+        print ( "\n " + str(result[1]))
+        sumatoria += result[1]
+    
+    print("LA SUMTORIA ES: " + str(sumatoria))
+    
 }
